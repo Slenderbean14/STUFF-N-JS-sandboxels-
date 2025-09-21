bd4bcd152a881215a93cca1d39192fd3b9f1f80d
Slenderbean14 <Slenderbean14@gmail.com>
2025-09-21 12:32:29 -0700

    Update Stuff'n.js

--- Stuff'n.js
+++ Stuff'n.js
@@ -1,21 +1,19 @@
-modFunction(() => {
-  elements.Recycle_Filter = {
-    color: "#8888ff",
-    category: "Filterings",
-    state: "solid",
-    density: 1000,
-    filterList: ["Mud", "Grass", "Wood"], // whitelist of allowed elements
-    tick: function(pixel) {
-      for (let i = 0; i < adjacentCoords.length; i++) {
-        let [dx, dy] = adjacentCoords[i];
-        let x = pixel.x + dx;
-        let y = pixel.y + dy;
-        let other = pixelMap[x]?.[y];
+elements.Recycle_Filter = {
+  color: "#8888ff",
+  category: "Filterings",
+  state: "solid",
+  density: 1000,
+  filterList: ["mud", "grass", "wood"], // lowercase names!
+  tick: function(pixel) {
+    for (let i = 0; i < adjacentCoords.length; i++) {
+      let [dx, dy] = adjacentCoords[i];
+      let x = pixel.x + dx;
+      let y = pixel.y + dy;
+      let other = pixelMap[x]?.[y];
 
-        if (other && elements[pixel.element].filterList.includes(other.element)) {
-          movePixel(other, dx, dy); // allow movement
-        }
+      if (other && elements[pixel.element].filterList.includes(other.element)) {
+        movePixel(other, dx, dy); // allow movement
       }
-    },
-  };
-});
+    }
+  }
+};
